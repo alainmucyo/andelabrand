ceba8d9b6e86a15125e95f0bca774a9afb9178bc
root <alainmucyo3@gmail.com>
2020-08-24 21:44:35 +0200

    Adding firebase

--- ui/js/index.js
+++ ui/js/index.js
@@ -22,3 +22,31 @@ function showSlides(n) {
     }
     slides[slideIndex - 1].style.display = "grid";
 }
+
+document.getElementById("contact_form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    var submitBtn = document.getElementById("submit");
+    var oldText = submitBtn.innerHTML;
+    submitBtn.innerHTML = "Sending message....";
+    var db = firebase.firestore();
+    db.collection("queries").add({
+        name, email, message
+    })
+        .then(function (docRef) {
+            submitBtn.innerHTML = oldText;
+            document.getElementById("contact_form").reset();
+            var alertSuccess=document.getElementsByClassName("alert-success")[0];
+            alertSuccess.innerHTML="Message sent successfully";
+            alertSuccess.style.display="block";
+        })
+        .catch(function (error) {
+            var alertDanger=document.getElementsByClassName("alert-danger")[0];
+            alertDanger.innerHTML="Error while sending the message.";
+            alertDanger.style.display="block";
+        });
+});
+
+
