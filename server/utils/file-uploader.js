1b93f8ea94e042fd357283a1607a319d58ad02f0
root <alainmucyo3@gmail.com>
2020-09-28 15:06:16 +0200

    Fixing a bug

--- server/utils/file-uploader.js
+++ server/utils/file-uploader.js
@@ -2,7 +2,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'storage/images/');
+        cb(null, 'storage/');
     },
     filename: function (req, file, cb) {
         let rand = Math.random().toString(36).substring(7);
