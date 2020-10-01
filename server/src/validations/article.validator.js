f9cd6bb8f0fab5d310bd8da9ff3215bca4b24ec3
root <alainmucyo3@gmail.com>
2020-10-01 12:05:08 +0200

    Integrate with front end

--- server/src/validations/article.validator.js
+++ server/src/validations/article.validator.js
@@ -2,7 +2,7 @@ import joi from 'joi'
 
 const articleValidation =  (body) => {
     const articleSchema = joi.object({
-        title: joi.string().required().min(3).max(20),
+        title: joi.string().required().min(3).max(100),
         content: joi.string().required().min(10),
     })
     return articleSchema.validate(body)
